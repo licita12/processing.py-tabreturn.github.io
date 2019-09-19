378cbc41bb975586e6cdc342a702127611942c8b
tabreturn <github@tabreturn.com>
2019-09-19 21:15:41 +1200

    - refactor microscopic sketch

--- lesson_09_(wip)/microscopic/amoeba.py
+++ lesson_09_(wip)/microscopic/amoeba.py
@@ -1,15 +1,21 @@
 class Amoeba:
-
+    
     def __init__(self, x, y, xspeed, yspeed, diameter):
         
         # instance variables
         
-        self.location = PVector(x, y)
+        self.location = PVector(x,y)
         self.velocity = PVector(xspeed, yspeed)
         self.topspeed = self.velocity.mag()
-        self.wobblex = random(xspeed*diameter/20, xspeed*diameter/20)
-        self.wobbley = random(yspeed*diameter/20, yspeed*diameter/20)
         self.diameter = float(diameter)
+        self.wobbleradii = []
+        
+        for i in range(4):
+            self.wobbleradii.append( PVector(
+              random(-xspeed*diameter/5, xspeed*diameter/5),
+              random(-yspeed*diameter/5, yspeed*diameter/5)
+            ))
+            
         self.t = 0.0
         
         # nucleus color
@@ -21,17 +27,18 @@ class Amoeba:
           self.diameter/random(2.5, 3),        # width & height
           nucleusfills[ int(random(len(nucleusfills))) ]
         ]
-        
+    
+    # methods
+    
     def ellips(self, x, y, t):
         x = x * cos(t)
         y = y * sin(t)
         return [x,y]
-        
+    
     def update(self):
-        self.location.add(self.velocity)
         
         # nucleus
-        
+
         fill(self.nucleus[3])
         noStroke()
         ellipse(
@@ -41,72 +48,66 @@ class Amoeba:
           self.nucleus[2]
         )
         
-        # cell
+        # shape
         
         fill(0x880099FF)
         stroke('#FFFFFF')
         strokeWeight(3)
-        v1 = self.ellips(1, 2, self.t)
-        v1x = v1[0] * self.wobblex
-        v1y = v1[1] * self.wobbley
-        v2 = self.ellips(2, 3, self.t)
-        v2x = v2[0] * self.wobblex
-        v2y = v2[1] * self.wobbley
-        v3 = self.ellips(5, 2, self.t)
-        v3x = v3[0] * self.wobblex
-        v3y = v3[1] * self.wobbley
-        v4 = self.ellips(2, 2, self.t)
-        v4x = v4[0] * self.wobblex
-        v4y = v4[1] * self.wobbley
+        self.location += self.velocity
         self.t += 0.05
         d = self.diameter
-        r = self.diameter/2.0
-        c = self.diameter/2/100*60
+        r = d/2.0
+        c = r*0.552
+        e = []
+        
+        for i in self.wobbleradii:
+            xy = self.ellips(i.x, i.y, self.t)
+            e.append( PVector(xy[0], xy[1]) )
+            
         beginShape()
         vertex(
           self.location.x+0, self.location.y+0
         )
         bezierVertex(
-          self.location.x+c+v1x, self.location.y+0+v1y,
-          self.location.x+r+v2x, self.location.y+r-c+v2y, 
+          self.location.x+c+e[0].x, self.location.y+e[0].y,
+          self.location.x+r+e[1].x, self.location.y+r-c+e[1].x,
           self.location.x+r, self.location.y+r
         )
         bezierVertex(
-          self.location.x+r+v2x*-1, self.location.y+r+c+v2y*-1,
-          self.location.x+c+v3x, self.location.y+d+v3y,
-          self.location.x+0, self.location.y+d
+          self.location.x+r+e[1].x*-1, self.location.y+r+c+e[1].y*-1,
+          self.location.x+c+e[2].x, self.location.y+d+e[2].y,
+          self.location.x, self.location.y+d
         )
         bezierVertex(
-          self.location.x+-c+v3x*-1, self.location.y+d+v3y*-1,
-          self.location.x+-r+v4x, self.location.y+r+c+v4y,
-          self.location.x+-r, self.location.y+r
+          self.location.x-c+e[2].x*-1, self.location.y+d+e[2].y*-1,
+          self.location.x-r+e[3].x, self.location.y+r+c+e[3].y,
+          self.location.x-r, self.location.y+r
         )
         bezierVertex(
-          self.location.x+-r+v4x*-1, self.location.y+r-c+v4y*-1,
-          self.location.x+-c+v1x*-1, self.location.y+0+v1y*-1,
-          self.location.x+0, self.location.y+0
+          self.location.x-r+e[3].x*-1, self.location.y+r-c+e[3].y*-1,
+          self.location.x-c+e[0].x*-1, self.location.y+e[0].y*-1,
+          self.location.x, self.location.y
         )
         endShape()
         
         # control points visualized
-        '''
+        
         stroke('#FF0000')
-        strokeWeight(1)
+        strokeWeight(2)
         line(
-          self.location.x+c+v1x, self.location.y+0+v1y, 
-          self.location.x+-c+v1x*-1, self.location.y+0+v1y*-1
+          self.location.x+c+e[0].x, self.location.y+e[0].y, 
+          self.location.x-c+e[0].x*-1, self.location.y+e[0].y*-1
         )
         line(
-          self.location.x+r+v2x, self.location.y+r-c+v2y, 
-          self.location.x+r+v2x*-1, self.location.y+r+c+v2y*-1
+          self.location.x+r+e[1].x, self.location.y+r-c+e[1].y, 
+          self.location.x+r+e[1].x*-1, self.location.y+r+c+e[1].y*-1
         )
         line(
-          self.location.x+c+v3x, self.location.y+d+v3y,
-          self.location.x+-c+v3x*-1, self.location.y+d+v3y*-1
+          self.location.x+c+e[2].x, self.location.y+d+e[2].y,
+          self.location.x-c+e[2].x*-1, self.location.y+d+e[2].y*-1
         )
         line(
-          self.location.x+-r+v4x, self.location.y+r+c+v4y,
-          self.location.x+-r+v4x*-1, self.location.y+r-c+v4y*-1
+          self.location.x-r+e[3].x, self.location.y+r+c+e[3].y,
+          self.location.x-r+e[3].x*-1, self.location.y+r-c+e[3].y*-1
         )
-        '''
         
--- lesson_09_(wip)/microscopic/microscopic.pyde
+++ lesson_09_(wip)/microscopic/microscopic.pyde
@@ -1,23 +1,22 @@
 from amoeba import Amoeba
 
-amoebas = []
-
 def setup():
     size(800,800)
-    global amoebas
-    
-    for i in range(8):      # number of amoeba
-        amoebas.append(Amoeba(
-          random(width),    # x
-          random(width),    # y
-          random(-0.5,0.5), # xspeed
-          random(-0.5,0.5), # yspeed
-          random(50,200)    # diameter
-        )) 
-    
+
+amoebas = []
+
+for i in range(8):      # number of amoeba
+    amoebas.append( Amoeba(
+      random(900),      # x
+      random(900),      # y
+      random(-0.5,0.5), # xspeed
+      random(-0.5,0.5), # yspeed
+      random(50,200)    # diameter
+    )) 
+
 def draw():
     background('#004477')
-    
+
     for a in amoebas:
         
         # mouse interaction
@@ -25,27 +24,32 @@ def draw():
         mouse = PVector(mouseX, mouseY-a.diameter/2)
         goto = PVector.sub(mouse, a.location)
         goto.div(10000)
+        
         if mousePressed:
             goto.mult(-1)
+            
         a.velocity.add(goto)
-        
-        # update
-        
         a.velocity.limit(a.topspeed)
         a.update()
         
-        # (start of the) collision detection 
-
-        strokeWeight(1)
-        stroke('#FF0000')
-        noFill()
-        ellipse(a.location.x, a.location.y+a.diameter/2, a.diameter, a.diameter)
+        # collision detection
         
         for b in amoebas:
             distance = dist(
-                         a.location.x, a.location.y +a.diameter/2, 
+                         a.location.x, a.location.y + a.diameter/2, 
                          b.location.x, b.location.y + b.diameter/2
                        )
+            
+            if distance < a.diameter/2 + b.diameter/2:
+                goto = PVector.sub(b.location, a.location)
+                goto.div(1000)
+                goto.mult(-1)
+                a.velocity.add(goto)
+            
+            # collisions visualized
+            
+            strokeWeight(1)
+            noFill()
             
             if distance < a.diameter/2 + b.diameter/2:
                 goto = PVector.sub(b.location, a.location)
@@ -55,24 +59,32 @@ def draw():
                 stroke('#00FF00')
             else: 
                 stroke('#FF0000')
-            
-             
+                
             line(
               a.location.x, a.location.y + a.diameter/2,
               b.location.x, b.location.y + b.diameter/2
             )
-
-# left-click to randomly add; right-click to randomly eliminate
         
-def mousePressed(): 
-    if mouseButton == LEFT:
-        amoebas.append(Amoeba(
-          random(width),    # x
-          random(width),    # y
-          random(-0.5,0.5), # xspeed
-          random(-0.5,0.5), # yspeed
-          random(50,200)    # diameter
-        )) 
+        # colliders visualized
+        
+        noFill()
+        stroke('#FF0000')
+        ellipse(a.location.x, a.location.y+a.diameter/2, a.diameter, a.diameter)
+
+# a-key to add an amoeba; d-key to remove one
+        
+def keyReleased():
     
-    if mouseButton == RIGHT:
+    print(keyCode)
+    
+    if keyCode == 65:
+        amoebas.append( Amoeba(
+        random(900),      # x
+        random(900),      # y
+        random(-0.5,0.5), # xspeed
+        random(-0.5,0.5), # yspeed
+        random(50,200)    # diameter
+        )) 
+
+    elif keyCode == 68 and len(amoebas) > 0:
         amoebas.pop()
